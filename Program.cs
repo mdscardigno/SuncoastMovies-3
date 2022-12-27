9481407de68a2af151b86f6a2f3940463c92bc91
Milagro Scardigno <mdscardigno@gmail.com>
2022-12-27 12:09:24 -0500

    Movie class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,36 @@
 
 namespace SuncoastMovies
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suncoast Movies");
+            // +--------------------------------+           +---------------------------+
+            // |            Movies              |           |         Ratings           |
+            // |                                |           |                           |
+            // |    Id                  SERIAL  |           |     Id           SERIAL   |
+            // |    Title               TEXT    | many  one |     Description  TEXT     |
+            // |    PrimaryDirector     TEXT    <----------->                           |
+            // |    YearReleased        INT     |           +---------------------------+
+            // |    Genre               TEXT    |
+            // |                                |
+            // +-------------^------------------+
+            //               | one
+            //               |
+            //               |
+            //               |
+            //               |
+            //               | many
+            //       +-------v----------------+               +-------------------------+
+            //       |        Roles           |               |          Actors         |
+            //       |                        | many      one |                         |
+            //       |   Id            SERIAL |<------------> |    Id          SERIAL   |
+            //       |   CharacterName TEXT   |               |    FullName    TEXT     |
+            //       |                        |               |    Birthday    DATE     |
+            //       |                        |               |                         |
+            //       +------------------------+               +-------------------------+
+
+        }
     }
-  }
 }
